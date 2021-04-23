9655fa4bad7ab38ab10402cf66f0d065477ff94c
Omhari <66086958+omharicu@users.noreply.github.com>
2021-04-24 01:31:26 +0530

    Update LCS bottom up version.cpp

--- dp/LCS bottom up version.cpp
+++ dp/LCS bottom up version.cpp
@@ -18,14 +18,14 @@ int32_t main()
 	m=s2.size();
 	int i;
 	int j;
-	for(i=0;i<=n;i++)
-	{
-		a[i][0]=0;
-	}
-	for(j=0;j<=m;j++)
-	{
-		a[0][j]=0;
-	}
+// 	for(i=0;i<=n;i++)
+// 	{
+// 		a[i][0]=0;
+// 	}
+// 	for(j=0;j<=m;j++)
+// 	{
+// 		a[0][j]=0;
+// 	}
 	for(i=1;i<=n;i++)
 	{
 		for(j=1;j<=m;j++)
